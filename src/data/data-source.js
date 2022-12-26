e3a16b6510047313e47c1173afcf5cec7995c37f
metiftikci <metiftikci@hotmail.com>
2022-12-26 23:24:48 +0300

    feat(children): add children example

--- src/data/data-source.js
+++ src/data/data-source.js
@@ -3,9 +3,18 @@ export const dataSource = [
   { label: "Header 2", field: "header2" },
   {
     label: "Content",
+    field: "content",
     children: [
       { label: "Name", field: "name" },
       { label: "Surname", field: "surname" },
+      {
+        label: "Prices",
+        field: "prices",
+        children: [
+          { label: "Id", field: "id" },
+          { label: "Price", field: "price" },
+        ],
+      },
     ],
   },
   { label: "Footer 1", field: "footer1" },
--- src/data/data.js
+++ src/data/data.js
@@ -5,14 +5,26 @@ export const data = {
     {
       name: "John1",
       surname: "Doe1",
+      prices: [
+        { id: 1, price: 10 },
+        { id: 2, price: 20 },
+      ],
     },
     {
       name: "John2",
       surname: "Doe2",
+      prices: [
+        { id: 3, price: 10 },
+        { id: 4, price: 20 },
+      ],
     },
     {
       name: "John3",
       surname: "Doe3",
+      prices: [
+        { id: 5, price: 10 },
+        { id: 6, price: 20 },
+      ],
     },
   ],
   footer1: "Footer 1",
